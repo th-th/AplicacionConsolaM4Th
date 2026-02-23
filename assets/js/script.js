ab28fbab2b5ad717087d8418561bbdcc183ec812
Thiare Valencia <lathiareconh@MacBook-Air-de-Thiare.local>
2026-02-23 13:27:12 -0300

    Se actualiza lección 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const nombre = prompt('Escriba su nombre');
 alert(`Bienvenido ${nombre}`);
 console.log(`Bienvenido/a ${nombre} a la aplicación en consola`);
 
-// Lección 2
+// Lección 2 y lección 4
 
 const numUno = (prompt('Ingrese un número'));
 const numDos = (prompt('Ingresa otro número'));
@@ -55,4 +55,52 @@ while (indice < mascotasFiltradas.length) {
     indice++;
 }
 
+// Lección 5
 
+/* 
+- Crear un objeto con propiedades y valores
+- Implementar métodos dentro de un objeto
+- Usar un arreglo de objetos y recorrerlo con map o forEach
+*/
+
+const mascota = {
+  nombre: 'Tito',
+  especie: 'Gato',
+  edad: 12,
+  // Método 
+  hablar: function () {
+    return `${this.nombre} maúlla`;
+  },
+};
+
+console.log(mascota.hablar());
+
+const paises = [
+  {
+    nombre: 'Chile',
+    idioma: 'español',
+  },
+  {
+    nombre: 'Australia',
+    idioma: 'inglés',
+  },
+  {
+    nombre: 'Islandia',
+    idioma: 'islandés',
+  },
+];
+
+// forEach no devuelve nada
+paises.forEach((pais) =>
+  console.log(`En ${pais.nombre} se habla ${pais.idioma}`),
+);
+
+// map devuelve un nuevo array
+const paisesPlaneta = paises.map((pais) => {
+  const nuevo = {
+    ...pais,
+    planeta: 'Tierra',
+  };
+  return nuevo;
+});
+console.log(paisesPlaneta);
